a738ecab74a848c0ff93b0e96352412e9ab90907
KIKIYUDINCAHYADI <cahyadik773@gmail.com>
2025-06-26 10:49:31 +0700

    membuat folder migrations

---
+++ migrations/schema.sql
@@ -0,0 +1,14 @@
+-- membuat Tabel Produk
+CREATE TABLE IF NOT EXISTS products (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  price INTEGER NOT NULL
+);
+
+-- membuat Tabel Transaksi
+CREATE TABLE IF NOT EXISTS transactions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  items TEXT NOT NULL,
+  total INTEGER NOT NULL,
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP
+);
